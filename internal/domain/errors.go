aae0a7e37553fb51e03d7535109cedbd951b64b5
Lucas Rodrigues <lucasrodrigues119816@gmail.com>
2025-04-08 15:10:50 -0300

    implementação do invoice

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -7,4 +7,7 @@ var (
 	ErrDuplicatedApiKey   = errors.New("duplicated api key")
 	ErrInvoiceNotFound    = errors.New("invoice not found")
 	ErrUnauthorizedAccess = errors.New("unauthorized access")
+	ErrInvalidAmount      = errors.New("invalid amount")
+	ErrInvalidDescription = errors.New("invalid description")
+	ErrInvalidStatus      = errors.New("invalid status")
 )
--- internal/domain/repository.go
+++ internal/domain/repository.go
@@ -6,3 +6,10 @@ type AccountRepository interface {
 	FindById(id string) (*Account, error)
 	Update(account *Account) error
 }
+
+type InvoiceRepository interface {
+	Save(invoice *Invoice) error
+	FindById(id string) (*Invoice, error)
+	FindByAccountID(AccountID string) (*[]Invoice, error)
+	UpdateStatus(invoice *Invoice) error
+}
